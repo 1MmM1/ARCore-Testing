89ea8004a25c9ddeab2ff3530a24e8317b9b6acf
1MmM1 <1MmM1@users.noreply.github.com>
2022-03-02 00:55:04 -0800

    Get app to build, though it crashes upon running

--- sceneform-ar/app/src/main/java/com/example/augmentedreality/MainActivity.java
+++ sceneform-ar/app/src/main/java/com/example/augmentedreality/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.augmentedreality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArFragment mArFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mArFragment = (ArFragment) ((Object) getSupportFragmentManager().findFragmentById(R.id.ar_fragment));
+        mArFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("TocoToucan.sfb"))
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable));
+        });
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode node = new AnchorNode(anchor);
+        TransformableNode transformableNode = new TransformableNode(mArFragment.getTransformationSystem());
+        transformableNode.setParent(node);
+        transformableNode.setRenderable(modelRenderable);
+        mArFragment.getArSceneView().getScene().addChild(node);
+        transformableNode.select();
     }
 }
